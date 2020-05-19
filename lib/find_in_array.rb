de0c617be8b5d6b77658bc71a54c94954bcc673f
Zana H <zhertica@gmail.com>
2020-05-19 04:29:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
   index = 0
   while index < array.length do
-    puts array[index] == value_to_find
+    if array[index] == value_to_find
+      return index
+    end
     index += 1
   end
 end
